60d55546619db22fd99b018d576850a81307d604
chd <homenet12@naver.com>
2021-07-14 22:50:06 +0900

    #13 다른페이지 가져오기

---
+++ lib/screens/edit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class EditPage extends StatelessWidget {
+  @override
+  Widget build(Object context) {
+    return Container(color: Colors.blue);
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'edit.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => EditPage()));
+        },
+        tooltip: '노트를 추가하려면 클릭하세요',
+        label: Text('메모 추가'),
+        icon: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
